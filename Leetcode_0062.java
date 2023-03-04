c8f793e766f03bb2eec3eeaac87922c0077e80a3
Shrey Srivastava <72334234+Shrey212001@users.noreply.github.com>
2023-03-04 19:16:52 +0530

    Min Cost Climbing Stairs

---
+++ Leetcode_0062.java
@@ -0,0 +1,59 @@
+/*
+
+Topic:- Min Cost Climbing Stairs
+
+Link:- https://leetcode.com/problems/min-cost-climbing-stairs/
+
+Problem:-
+
+You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
+
+Example 1:
+
+Input: cost = [10,15,20]
+Output: 15
+Explanation: You will start at index 1.
+- Pay 15 and climb two steps to reach the top.
+The total cost is 15.
+Example 2:
+
+Input: cost = [1,100,1,1,1,100,1,1,100,1]
+Output: 6
+Explanation: You will start at index 0.
+- Pay 1 and climb two steps to reach index 2.
+- Pay 1 and climb two steps to reach index 4.
+- Pay 1 and climb two steps to reach index 6.
+- Pay 1 and climb one step to reach index 7.
+- Pay 1 and climb two steps to reach index 9.
+- Pay 1 and climb one step to reach the top.
+The total cost is 6.
+ 
+
+Constraints:
+
+2 <= cost.length <= 1000
+0 <= cost[i] <= 999
+
+
+
+Solution:-
+*/
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+
+        int prev=cost[0],prev2=0,current;
+        for(int i=1;i<cost.length;i++){
+            current=Math.min(prev2+cost[i],prev+cost[i]);
+            prev2=prev;
+            prev=current;
+        }
+        
+        return Math.min(prev,prev2);
+
+
+    }
+}
