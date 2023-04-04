7579c3e2cf60e19b597342af4ae284649f58c9e5
Shrey Srivastava <72334234+Shrey212001@users.noreply.github.com>
2023-04-04 12:32:20 +0530

    Maximize Sum Of Array After K Negations

---
+++ Leetcode_0092.java
@@ -0,0 +1,57 @@
+/*
+
+Topic:- Maximize Sum Of Array After K Negations
+
+Link:- https://leetcode.com/problems/maximize-sum-of-array-after-k-negations/
+
+Problem:-
+
+Given an integer array nums and an integer k, modify the array in the following way:
+
+choose an index i and replace nums[i] with -nums[i].
+You should apply this process exactly k times. You may choose the same index i multiple times.
+
+Return the largest possible sum of the array after modifying it in this way.
+
+Example 1:
+
+Input: nums = [4,2,3], k = 1
+Output: 5
+Explanation: Choose index 1 and nums becomes [4,-2,3].
+Example 2:
+
+Input: nums = [3,-1,0,2], k = 3
+Output: 6
+Explanation: Choose indices (1, 2, 2) and nums becomes [3,1,0,2].
+Example 3:
+
+Input: nums = [2,-3,-1,5,-4], k = 2
+Output: 13
+Explanation: Choose indices (1, 4) and nums becomes [2,3,-1,5,4].
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-100 <= nums[i] <= 100
+1 <= k <= 104
+
+
+
+
+Solution:-
+*/
+class Solution {
+       public int largestSumAfterKNegations(int[] A, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        
+        for(int x: A) pq.add(x);
+        while( K--  > 0) pq.add(-pq.poll());
+  
+        int sum  = 0;
+        for(int i = 0; i < A.length; i++){
+            sum += pq.poll();
+        }
+        return sum;
+    }
+}
