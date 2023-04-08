397a1294c74f47c66940a5d9d5b40f65f9957e02
Shrey Srivastava <72334234+Shrey212001@users.noreply.github.com>
2023-04-08 15:59:29 +0530

    Valid Boomerang

---
+++ Leetcode_0096.java
@@ -0,0 +1,37 @@
+/*
+
+Topic:- Valid Boomerang
+ 
+Link:- https://leetcode.com/problems/valid-boomerang/
+
+Problem:-
+
+Given an array points where points[i] = [xi, yi] represents a point on the X-Y plane, return true if these points are a boomerang.
+
+A boomerang is a set of three points that are all distinct and not in a straight line.
+
+Example 1:
+
+Input: points = [[1,1],[2,3],[3,2]]
+Output: true
+Example 2:
+
+Input: points = [[1,1],[2,2],[3,3]]
+Output: false
+ 
+Constraints:
+
+points.length == 3
+points[i].length == 2
+0 <= xi, yi <= 100
+
+
+
+
+Solution:-
+*/
+class Solution {
+       public boolean isBoomerang(int[][] p) {
+        return (p[0][0] - p[1][0]) * (p[0][1] - p[2][1]) != (p[0][0] - p[2][0]) * (p[0][1] - p[1][1]);
+    }
+}
