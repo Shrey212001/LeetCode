8e360403317c654568348724fbf783de4dd976ce
Shrey Srivastava <72334234+Shrey212001@users.noreply.github.com>
2023-04-12 19:29:20 +0530

    Relative Sort Array

---
+++ Leetcode_0100.java
@@ -0,0 +1,51 @@
+/*
+
+Topic:-  Relative Sort Array
+
+Link:- https://leetcode.com/problems/relative-sort-array/
+
+Problem:-
+
+Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+
+Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2. Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order. 
+
+Example 1:
+
+Input: arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
+Output: [2,2,2,1,4,3,3,9,6,7,19]
+Example 2:
+
+Input: arr1 = [28,6,22,8,44,17], arr2 = [22,28,8,6]
+Output: [22,28,8,6,17,44]
+ 
+Constraints:
+
+1 <= arr1.length, arr2.length <= 1000
+0 <= arr1[i], arr2[i] <= 1000
+All the elements of arr2 are distinct.
+Each arr2[i] is in arr1.
+
+
+
+
+Solution:-
+*/
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] cnt = new int[1001];
+        for(int n : arr1) cnt[n]++;
+        int i = 0;
+        for(int n : arr2) {
+            while(cnt[n]-- > 0) {
+                arr1[i++] = n;
+            }
+        }
+        for(int n = 0; n < cnt.length; n++) {
+            while(cnt[n]-- > 0) {
+                arr1[i++] = n;
+            }
+        }
+        return arr1;
+    }
+}
