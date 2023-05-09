58e5f78b8d4b343f6bbc65540eee7dcb3712b947
Shrey Srivastava <72334234+Shrey212001@users.noreply.github.com>
2023-05-09 20:02:40 +0530

    Minimum Subsequence in Non-Increasing Order

---
+++ Leetcode_0127.java
@@ -0,0 +1,52 @@
+/*
+
+Topic:- Minimum Subsequence in Non-Increasing Order
+
+Link:- https://leetcode.com/problems/minimum-subsequence-in-non-increasing-order/description/
+
+Problem:-
+
+Given the array nums, obtain a subsequence of the array whose sum of elements is strictly greater than the sum of the non included elements in such subsequence. 
+
+If there are multiple solutions, return the subsequence with minimum size and if there still exist multiple solutions, return the subsequence with the maximum total sum of all its elements. A subsequence of an array can be obtained by erasing some (possibly zero) elements from the array. 
+
+Note that the solution with the given constraints is guaranteed to be unique. Also return the answer sorted in non-increasing order.
+
+Example 1:
+
+Input: nums = [4,3,10,9,8]
+Output: [10,9] 
+Explanation: The subsequences [10,9] and [10,8] are minimal such that the sum of their elements is strictly greater than the sum of elements not included. However, the subsequence [10,9] has the maximum total sum of its elements. 
+Example 2:
+
+Input: nums = [4,4,7,6,7]
+Output: [7,7,6] 
+Explanation: The subsequence [7,7] has the sum of its elements equal to 14 which is not strictly greater than the sum of elements not included (14 = 4 + 4 + 6). Therefore, the subsequence [7,6,7] is the minimal satisfying the conditions. Note the subsequence has to be returned in non-decreasing order.  
+ 
+Constraints:
+
+1 <= nums.length <= 500
+1 <= nums[i] <= 100
+
+
+
+
+Solution:-
+*/
+      class Solution {
+    public List<Integer> minSubsequence(int[] nums) {
+               Arrays.sort(nums);
+        int sum = 0, temp = 0;
+        for (int i: nums) sum += i;
+        
+        List<Integer> ans = new ArrayList<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            sum -= nums[i];
+            temp += nums[i];
+            ans.add(nums[i]);
+            if (temp > sum) break;
+        }
+
+        return ans; 
+    }
+}
