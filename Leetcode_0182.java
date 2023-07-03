75f5d768b3e5a46a719f3e1d4bca65f7f92b43e1
Shrey Srivastava <72334234+Shrey212001@users.noreply.github.com>
2023-07-03 21:54:22 +0530

    Find the Middle Index in Array

---
+++ Leetcode_0182.java
@@ -0,0 +1,63 @@
+/*
+
+Topic:- Find the Middle Index in Array
+
+Link:- https://leetcode.com/problems/find-the-middle-index-in-array/description/
+
+Problem:-
+
+Given a 0-indexed integer array nums, find the leftmost middleIndex (i.e., the smallest amongst all the possible ones).
+
+A middleIndex is an index where nums[0] + nums[1] + ... + nums[middleIndex-1] == nums[middleIndex+1] + nums[middleIndex+2] + ... + nums[nums.length-1].
+
+If middleIndex == 0, the left side sum is considered to be 0. Similarly, if middleIndex == nums.length - 1, the right side sum is considered to be 0.
+
+Return the leftmost middleIndex that satisfies the condition, or -1 if there is no such index.
+
+Example 1:
+
+Input: nums = [2,3,-1,8,4]
+Output: 3
+Explanation: The sum of the numbers before index 3 is: 2 + 3 + -1 = 4
+The sum of the numbers after index 3 is: 4 = 4
+Example 2:
+
+Input: nums = [1,-1,4]
+Output: 2
+Explanation: The sum of the numbers before index 2 is: 1 + -1 = 0
+The sum of the numbers after index 2 is: 0
+Example 3:
+
+Input: nums = [2,5]
+Output: -1
+Explanation: There is no valid middleIndex. 
+
+Constraints:
+
+1 <= nums.length <= 100
+-1000 <= nums[i] <= 1000
+
+
+
+
+Solution:-
+*/
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int lsum[] = new int[nums.length]; 
+        int rsum[] = new int[nums.length]; 
+        lsum[0]=nums[0];
+        rsum[nums.length-1] = nums[nums.length-1];
+        for(int i=1; i<nums.length ;i++){
+            lsum[i] = lsum[i-1] + nums[i]; 
+        }
+        for(int i=nums.length-2; i>=0; i--){
+            rsum[i] = rsum[i+1] + nums[i]; 
+        }
+        for(int i=0; i<nums.length;i++){
+            if(lsum[i]==rsum[i])
+            return i; 
+        }
+        return -1; 
+    }
+}
