e4ca94c5286f0349f61f8e98a6aefae3800ea180
Shrey Srivastava <72334234+Shrey212001@users.noreply.github.com>
2023-07-04 17:02:28 +0530

    Count Special Quadruplets

---
+++ Leetcode_0183.java
@@ -0,0 +1,64 @@
+/*
+
+Topic:- Count Special Quadruplets
+
+Link:- https://leetcode.com/problems/count-special-quadruplets/description/
+
+Problem:-
+
+Given a 0-indexed integer array nums, return the number of distinct quadruplets (a, b, c, d) such that:
+
+nums[a] + nums[b] + nums[c] == nums[d], and
+a < b < c < d
+ 
+Example 1:
+
+Input: nums = [1,2,3,6]
+Output: 1
+Explanation: The only quadruplet that satisfies the requirement is (0, 1, 2, 3) because 1 + 2 + 3 == 6.
+Example 2:
+
+Input: nums = [3,3,6,4,5]
+Output: 0
+Explanation: There are no such quadruplets in [3,3,6,4,5].
+Example 3:
+
+Input: nums = [1,1,1,3,5]
+Output: 4
+Explanation: The 4 quadruplets that satisfy the requirement are:
+- (0, 1, 2, 3): 1 + 1 + 1 == 3
+- (0, 1, 3, 4): 1 + 1 + 3 == 5
+- (0, 2, 3, 4): 1 + 1 + 3 == 5
+- (1, 2, 3, 4): 1 + 1 + 3 == 5
+ 
+Constraints:
+
+4 <= nums.length <= 50
+1 <= nums[i] <= 100
+
+
+
+
+Solution:-
+*/
+class Solution {
+    public int countQuadruplets(int[] nums) {
+        int res = 0;
+        int len = nums.length;
+        
+        Map<Integer, Integer> count = new HashMap<>();
+        count.put(nums[len-1] - nums[len-2], 1);
+        
+        for (int b = len - 3; b >= 1; b--) {
+            for (int a = b - 1; a >= 0; a--) {
+                res += count.getOrDefault(nums[a] + nums[b], 0);
+            }
+            
+            for (int x = len - 1; x > b; x--) {
+                count.put(nums[x] - nums[b], count.getOrDefault(nums[x] - nums[b], 0) + 1);
+            }
+        }
+        
+        return res;
+    }
+}
