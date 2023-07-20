d689f9a2ad3d5e03aff7cc425f86be6adbc128e4
Shrey Srivastava <72334234+Shrey212001@users.noreply.github.com>
2023-07-20 17:03:08 +0530

    Check if Every Row and Column Contains All Numbers

---
+++ Leetcode_0199.java
@@ -0,0 +1,50 @@
+/*
+
+Topic:-  Check if Every Row and Column Contains All Numbers
+
+Link:- https://leetcode.com/problems/check-if-every-row-and-column-contains-all-numbers/description/
+
+Problem:-
+
+An n x n matrix is valid if every row and every column contains all the integers from 1 to n (inclusive).
+
+Given an n x n integer matrix matrix, return true if the matrix is valid. Otherwise, return false.
+
+Example 1:
+
+Input: matrix = [[1,2,3],[3,1,2],[2,3,1]]
+Output: true
+Explanation: In this case, n = 3, and every row and column contains the numbers 1, 2, and 3.
+Hence, we return true.
+Example 2:
+
+Input: matrix = [[1,1,1],[1,2,3],[1,2,3]]
+Output: false
+Explanation: In this case, n = 3, but the first row and the first column do not contain the numbers 2 or 3.
+Hence, we return false.
+ 
+Constraints:
+
+n == matrix.length == matrix[i].length
+1 <= n <= 100
+1 <= matrix[i][j] <= n
+
+
+
+
+Solution:-
+*/
+class Solution {
+    public boolean checkValid(int[][] matrix) {
+        for (int r = 0, n = matrix.length; r < n; ++r) {
+            Set<Integer> row = new HashSet<>();
+            Set<Integer> col = new HashSet<>();
+            for (int c = 0; c < n; ++c) {
+                if (!row.add(matrix[r][c]) || !col.add(matrix[c][r])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
