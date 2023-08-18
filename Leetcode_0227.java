38f9584d208231a72359dfd38739ab25c9c42259
Shrey Srivastava <72334234+Shrey212001@users.noreply.github.com>
2023-08-18 09:30:26 +0530

    Check Distances Between Same Letters

---
+++ Leetcode_0227.java
@@ -0,0 +1,69 @@
+/*
+
+Topic:- Check Distances Between Same Letters
+
+Link:- https://leetcode.com/problems/check-distances-between-same-letters/description/
+
+Problem:-
+
+You are given a 0-indexed string s consisting of only lowercase English letters, where each letter in s appears exactly twice. You are also given a 0-indexed integer array distance of length 26.
+
+Each letter in the alphabet is numbered from 0 to 25 (i.e. 'a' -> 0, 'b' -> 1, 'c' -> 2, ... , 'z' -> 25).
+
+In a well-spaced string, the number of letters between the two occurrences of the ith letter is distance[i]. If the ith letter does not appear in s, then distance[i] can be ignored.
+
+Return true if s is a well-spaced string, otherwise return false.
+
+Example 1:
+
+Input: s = "abaccb", distance = [1,3,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+Output: true
+Explanation:
+- 'a' appears at indices 0 and 2 so it satisfies distance[0] = 1.
+- 'b' appears at indices 1 and 5 so it satisfies distance[1] = 3.
+- 'c' appears at indices 3 and 4 so it satisfies distance[2] = 0.
+Note that distance[3] = 5, but since 'd' does not appear in s, it can be ignored.
+Return true because s is a well-spaced string.
+Example 2:
+
+Input: s = "aa", distance = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+Output: false
+Explanation:
+- 'a' appears at indices 0 and 1 so there are zero letters between them.
+Because distance[0] = 1, s is not a well-spaced string.
+ 
+Constraints:
+
+2 <= s.length <= 52
+s consists only of lowercase English letters.
+Each letter appears in s exactly twice.
+distance.length == 26
+0 <= distance[i] <= 50
+
+
+
+
+Solution:-
+*/
+class Solution {
+    public boolean checkDistances(String s, int[] distance) {
+        HashSet<Character> set = new HashSet<>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int ind = 0;
+            if(set.contains(c)) {
+                continue;
+            }
+            for(int j = i+1; j < s.length(); j++){ 
+                if(s.charAt(j) == c) {
+                    ind = j;
+                }
+            }
+            set.add(c);
+            if(ind-i-1 != distance[c - 'a']){ 
+                return false;
+            }
+        }
+        return true;
+    }
+}
